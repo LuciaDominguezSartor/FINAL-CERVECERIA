fb540979a3e102427dfd4775e43ecb37591ecfe2
Lucia Dominguez <luciadominguezsartor@gmail.com>
2023-08-09 21:15:25 -0300

    Crud3

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,7 @@ router.get('/', async function(req, res, next) {
     } else {
         return {
             ...novedad,
-            imagen: '/images/noimage.jpg'
+            imagen: './images/noimage.jpg'
         }
     }
 });
